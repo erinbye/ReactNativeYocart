54105c3b3b9bf85d06cd458edf7c323735e430e7
Erin <erinbye99@gmail.com>
2020-07-18 18:55:39 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+
+class NavButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: props.text,
+    };
+  }
+
+  render() {
+    return (
+      <TouchableOpacity activeOpacity={0.8} style={styles.navButtonContainer}>
+        <Text style={styles.navButtonText}>{this.state.text}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titleText}>Yo-cart!</Text>
+      <NavButton text="Grocery List" />
+      <NavButton text="Recipes" />
+      <NavButton text="Inventory" />
+      <NavButton text="Settings" />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +35,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titleText: {
+    fontSize: 80,
+    fontWeight: "bold",
+    marginBottom: 30,
+  },
+  navButtonContainer: {
+    elevation: 8,
+    backgroundColor: "#da96e7",
+    borderRadius: 30,
+    paddingVertical: 20,
+    height: 100,
+    width: 300,
+    alignItems: "center",
+    margin: 10,
+  },
+  navButtonText: {
+    fontSize: 40,
+    fontFamily: "Times New Roman",
+    color: "#fff",
   },
 });
